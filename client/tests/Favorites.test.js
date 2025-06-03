29c0c73c5246a6792f85ae10c50c370cc0da2f74
aboggan <alexisboggan@gmail.com>
2025-06-02 21:23:55 -0300

    test(favorites): wrap useEffect calls in act() to fix async state updates

--- client/tests/Favorites.test.js
+++ client/tests/Favorites.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, waitFor, act } from '@testing-library/react';
 import { FavoritesProvider, FavoritesContext } from '../src/context/FavoritesContext';
+
+jest.mock('../src/utils/getApiUrl', () => ({
+  getApiUrl: () => 'http://localhost:4000',
+}));
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve([]),
+  })
+);
 
 describe('FavoritesContext', () => {
   beforeEach(() => {
@@ -8,38 +19,44 @@ describe('FavoritesContext', () => {
     jest.clearAllMocks();
   });
 
-  test('should initialize with an empty favorites list', () => {
+  test('should initialize with an empty favorites list', async () => {
     let favoritesValue;
-    render(
-      <FavoritesProvider>
-        <FavoritesContext.Consumer>
-          {(value) => {
-            favoritesValue = value;
-            return null;
-          }}
-        </FavoritesContext.Consumer>
-      </FavoritesProvider>
-    );
+    await act(async () => {
+      render(
+        <FavoritesProvider>
+          <FavoritesContext.Consumer>
+            {(value) => {
+              favoritesValue = value;
+              return null;
+            }}
+          </FavoritesContext.Consumer>
+        </FavoritesProvider>
+      );
+    });
 
     expect(favoritesValue.favorites).toEqual([]);
   });
 
   test('should add a city to favorites', async () => {
     let favoritesValue;
-    render(
-      <FavoritesProvider>
-        <FavoritesContext.Consumer>
-          {(value) => {
-            favoritesValue = value;
-            return null;
-          }}
-        </FavoritesContext.Consumer>
-      </FavoritesProvider>
-    );
+    await act(async () => {
+      render(
+        <FavoritesProvider>
+          <FavoritesContext.Consumer>
+            {(value) => {
+              favoritesValue = value;
+              return null;
+            }}
+          </FavoritesContext.Consumer>
+        </FavoritesProvider>
+      );
+    });
 
     const newCity = { name: 'London', id: 2643743 };
 
-    favoritesValue.addToFavorites(newCity);
+    await act(async () => {
+      favoritesValue.addToFavorites(newCity);
+    });
 
     await waitFor(() => {
       expect(favoritesValue.favorites.some(f => f.name === 'London')).toBe(true);
@@ -48,27 +65,32 @@ describe('FavoritesContext', () => {
 
   test('should remove a city from favorites', async () => {
     let favoritesValue;
-    render(
-      <FavoritesProvider>
-        <FavoritesContext.Consumer>
-          {(value) => {
-            favoritesValue = value;
-            return null;
-          }}
-        </FavoritesContext.Consumer>
-      </FavoritesProvider>
-    );
+    await act(async () => {
+      render(
+        <FavoritesProvider>
+          <FavoritesContext.Consumer>
+            {(value) => {
+              favoritesValue = value;
+              return null;
+            }}
+          </FavoritesContext.Consumer>
+        </FavoritesProvider>
+      );
+    });
 
     const city = { name: 'London', id: 2643743 };
 
-    favoritesValue.addToFavorites(city);
+    await act(async () => {
+      favoritesValue.addToFavorites(city);
+    });
 
     await waitFor(() => {
       expect(favoritesValue.favorites.some(f => f.name === 'London')).toBe(true);
     });
 
-    // Remove it
-    favoritesValue.removeFromFavorites({ ...city, _id: 'mock-id' });
+    await act(async () => {
+      favoritesValue.removeFromFavorites({ ...city, _id: 'mock-id' });
+    });
 
     await waitFor(() => {
       expect(favoritesValue.favorites.some(f => f.name === 'London')).toBe(false);
