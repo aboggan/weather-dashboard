e567ecc8ee241b17040799b4c6946b438239b68d
aboggan <alexisboggan@gmail.com>
2025-06-03 13:23:00 -0300

    feat: enhance CORS middleware to allow dynamic origin configuration

--- server/src/middleware/corsMiddleware.js
+++ server/src/middleware/corsMiddleware.js
@@ -1,8 +1,17 @@
 import cors from 'cors';
-import config from '../config/config.js';
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : [];
 
 const corsOptions = {
-  origin: config.clientUrl,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 };
 
